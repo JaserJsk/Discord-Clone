9850f363b01c850ac4a8f4ce74f21aa457d10a2c
Jaser Jsk <Jaser.Jsk@gmail.com>
2021-09-12 17:01:27 +0200

    Removed Github Workflows

--- discord-clone-app/src/components/Login.js
+++ discord-clone-app/src/components/Login.js
@@ -1,26 +1,24 @@
-import './Login.css';
+import "./Login.css";
 
-import React from 'react';
-import Logo from '../assets/logo.svg';
-import { auth, provider } from '../config/firebase';
-import { Button } from '@material-ui/core'
+import React from "react";
+import Logo from "../assets/logo.svg";
+import { auth, provider } from "../config/firebase";
+import { Button } from "@material-ui/core";
 
 function Login() {
-    const signIn = () => {
-        auth.signInWithPopup(provider).catch(
-            error => alert(error.message)
-        );
-    };
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
 
-    return (
-        <div className="login">
-            <div className="login-logo">
-                <img src={Logo} alt="Discord Clone" />
-            </div>
+  return (
+    <div className="login">
+      <div className="login-logo">
+        <img src={Logo} alt="Discord Clone" />
+      </div>
 
-            <Button onClick={signIn}>Sign In</Button>
-        </div>
-    );
+      <Button onClick={signIn}>Sign In</Button>
+    </div>
+  );
 }
 
 export default Login;
